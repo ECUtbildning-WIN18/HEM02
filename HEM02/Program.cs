8ab43b10e3af65a87d065110bbb1b18e2c1546ef
Lars Bylund <lars.bylund@hotmail.com>
2018-09-10 22:43:06 +0200

    Initial commit

--- HEM02/Program.cs
+++ HEM02/Program.cs
@@ -8,5 +8,35 @@ namespace HEM02
         {
             Console.WriteLine("Hello World!");
         }
+
+
+        class Car
+        {
+            public string Brand { get; }
+            public string Model { get; }
+
+            public Engine engine { get; }
+
+            public Person owner { get; }
+
+            public Car(string Brand, string model, Engine engine, Person owner)
+            {
+                Brand = brand;
+                Model = model;
+                Engine = engine;
+                Owner = owner;
+            }
+        }
+
+        class Person
+        {
+            public string FirstName;
+            public string LastName;
+            public Person()
+            {
+
+            }
+
+        }
     }
 }
