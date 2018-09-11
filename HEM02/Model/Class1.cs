7f8aa249328dbf1715de3eb3569a31ecc802292a
Paula Hernandez <paula.frost@hotmail.com>
2018-09-11 15:23:56 +0200

    Add classes

---
+++ HEM02/Model/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HEM02.Model
+{
+    class Car
+    {
+
+    }
+
+
+    class Engine
+    {
+
+    }
+
+    class SoundSystem
+    {
+        string TurnOn;
+        string TurnOff;
+        string VolumeUpp;
+        string VolumeDown;
+
+    }
+
+}
