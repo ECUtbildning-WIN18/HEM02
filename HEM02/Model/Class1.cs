02474775b3647ff7f4eebd240b1177a6d81801cb
Paula Hernandez <paula.frost@hotmail.com>
2018-09-11 16:07:43 +0200

    Class changes 2

--- HEM02/Model/Class1.cs
+++ HEM02/Model/Class1.cs
@@ -6,13 +6,41 @@ namespace HEM02.Model
 {
     class Car
     {
+        private string Brand { get; }
+        private string Model { get; }
 
+        public Engine Engine { get; }
+        public Person Owner { get; }
+
+        public Car(string _Brand, string _Model, Engine _engine, Person _Owner)
+        {
+            Brand = _Brand;
+            Model = _Model;
+            Engine = _engine;
+            Owner = _Owner;
+        }
     }
 
 
     class Engine
     {
+        private string Type { get; }
+        private int Output { get; }
+        private string SerialNumber { get; }
+    }
 
+    class Person
+    {
+        private string FirstName { get; }
+        private string LastName { get; }
+        private int PersonalID { get; }
+
+        public Person(string _FirstName, string _LastName, int _PersonalID)
+        {
+            _FirstName = FirstName;
+            _LastName = LastName;
+            _PersonalID = PersonalID;
+        }
     }
 
     class SoundSystem
@@ -21,6 +49,11 @@ namespace HEM02.Model
         string TurnOff;
         string VolumeUpp;
         string VolumeDown;
+
+    }
+
+    class LudicrousSpeed
+    {
 
     }
 
