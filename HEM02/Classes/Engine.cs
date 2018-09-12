29e15ce34d83ce8eea58883c693889ce769a5438
Wade Tamim <w.tamim@hotmail.com>
2018-09-12 21:16:55 +0200

    Changes Made

---
+++ HEM02/Classes/Engine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HEM02.Classes
+{
+    class Engine
+    {
+        public double SerialNumber { get; set; }
+        double HorsePower { get; set; } = 3000;
+
+        public double Speed { get; } = 0; 
+        
+        
+        
+        public void Accelerate()
+        {
+
+        }
+
+
+        public void Breaking()
+        {
+
+
+        }
+
+
+        public void RaceButton()
+        {
+
+
+        }
+    }
+}
--- HEM02/Program.cs
+++ HEM02/Program.cs
@@ -1,63 +1,76 @@
-﻿using System;
+﻿using HEM02.Classes;
+using System;
 
 namespace HEM02
 {
-    class car
-    {
-        public string Owner { get; set; }
-        public string Registration { get; set; }
-        public string Color { get; set; }
-
-    }
-
-    class Engine
-    {
-        double SerialNumber { get; set; }
-        double HorsePower { get; set; }
-        public double Speed { get; set; } 
-
-        public void Accelerate ()
-        {
-            Speed = Speed + 10;
-        }
-        public void Breaking ()
-        {
-            Speed = Speed - 10;
-
-        }
-        public void RaceButton ()
-        {
-            Speed = Speed + 20;
-
-        }
-    }
-    class Audio
-    {
-        public bool Switch { get; set; }
-        public double Volume { get; set; }
-
-        public void OnOff ()
-        {
-            Switch = !Switch;
-
-        }
-        public void IncreaseVolume ()
-        {
-            Volume = Volume + 5;
-
-
-        }
-        public void Decrease ()
-        {
-            Volume = Volume - 5;
-        }
-
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
+
+            car car;
+            car = new car();
+            Console.WriteLine("Please enter the owner's name :");
+            car.Owner = Console.ReadLine();
+            Console.WriteLine("Please enter the car's color :");
+            car.Color = Console.ReadLine();
+            Console.WriteLine("Please enter the car's registration");
+            car.Registration = Console.ReadLine();
+
+            Console.WriteLine(car.Owner);
+            Console.WriteLine(car.Color);
+            Console.WriteLine(car.Registration);
+
+            Audio audio = new Audio();
+            Console.WriteLine("\n\n************************************");
+            Console.WriteLine("Audio options :\n1.Turn it off/on.\n2.Increase volume.\n3.Decrease volume.");
+            int audioAnswer = 0;
+                audioAnswer = int.Parse(Console.ReadLine());
+
+            switch(audioAnswer)
+            {
+                case 1:
+                    audio.OnOff();
+                    break;
+                case 2:
+                    audio.IncreaseVolume();
+                    break;
+                case 3:
+                    audio.DecreaseVolume();
+                    break;
+
+            }
+
+            Engine engine = new Engine();
+            Console.WriteLine("Enter the car's Serialnumber");
+
+            engine.SerialNumber =double.Parse( Console.ReadLine());
+
+            Console.WriteLine("1.To accelerate.\n2.To break.\n3.Race mode");
+            int engineAnswer = 0;
+            switch(engineAnswer)
+                {
+                case 1:
+                    engine.Accelerate();
+                    break;
+                case 2:
+                    engine.Breaking();
+                    break;
+                case 3:
+                    engine.RaceButton();
+                    break;
+               
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+
+
 
         }
     }
