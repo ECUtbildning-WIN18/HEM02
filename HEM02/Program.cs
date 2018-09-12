18aa502db5a1aafb22d2ba3ba840d07e87bbdd61
Nayomi Arvell <nayomi.arvell@gmail.com>
2018-09-12 22:00:09 +0200

    HEM02, teslauppgift

--- HEM02/Program.cs
+++ HEM02/Program.cs
@@ -6,7 +6,30 @@ namespace HEM02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Owner owner = new Owner("Robert", "Tublén");
+            Car tesla = new Car("Tesla", "3", new Engine(), new SoundSystem(true, 0),owner);
+
+            Console.WriteLine("Welcome to your " + tesla.Brand + " " + tesla.Model + ", " + tesla.Owner.FirstName + "\n" + "Would you like to turn on some music? (y/n)");
+            string userInput = Console.ReadLine();
+
+            if(userInput == "y")
+            {
+                tesla.Music.TurnedOn = true;
+                Console.WriteLine("Oh, how nice. Your favourite song 'Rosa helikopter' is now playing.");
+
+            }
+
+            Console.WriteLine("You are currently traveling in " + tesla.Speed + "km/h, would you like to enter loudicrous speed? (y/n)");
+            userInput = Console.ReadLine();
+
+            if (userInput == "y")
+            {
+                tesla.loudicrousSpeed = true;
+                tesla.Accelerate();
+                Console.WriteLine("You are currently traveling in " + tesla.Speed + "km/h.");
+            }
+            
+                Console.ReadKey();
         }
     }
 }
