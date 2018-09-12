2dba493f13cf83b0c18238fcb9fb00b4c2e207b4
Ryan Cockram <ryan.cockram@ecutbildning.se>
2018-09-12 16:03:42 +0200

    compelted engine

--- HEM02/Engine.cs
+++ HEM02/Engine.cs
@@ -3,8 +3,24 @@ namespace HEM02
 {
     public class Engine
     {
-        public Engine()
+        string Model            { get; }
+        int Serial              { get; }
+        DateTime CreationDate   { get; }
+        bool EngineEnabled      { get; }
+
+        public Engine(string model, int serial, DateTime creationDate)
         {
+            model = Model;
+            serial = Serial;
+            creationDate = CreationDate;
+        }
+
+        public Engine(string model, int serial, DateTime creationDate, bool engineEnabled)
+        {
+            model = Model;
+            serial = Serial;
+            creationDate = CreationDate;
+            engineEnabled = EngineEnabled;
         }
     }
 }
